0aaf7aa50dbfcb33e4dc60d4bf0aa42df9f0d44c
Nick Schaefer <wildernns@gmail.com>
2023-10-02 08:17:27 -0400

    Initial Commit

---
+++ Summits/SummitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummitsApp.swift
+//  Summits
+//
+//  Created by Nick on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SummitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
